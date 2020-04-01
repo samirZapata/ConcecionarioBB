70de75c446b704f5e225e5c8e0f1371655636a0a
yonier <yonier@yonier-X540LA>
2020-03-31 23:32:50 -0500

    version 1.0 estoy en el proceso de rediseñado del proyecto empezare a implementar el registro con listas enlazadas y la enlazo con la BD

---
+++ src/view/Registrarse.java
@@ -0,0 +1,32 @@
+package view;
+
+/**
+ *
+ * @author yonier
+ */
+public class Registrarse extends javax.swing.JPanel {
+
+    public Registrarse() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 819, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 435, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
